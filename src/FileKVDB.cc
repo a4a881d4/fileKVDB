af71a38b4d9973e81ff598f7bf755438c69f3f98
ubuntu <ubuntu@ubuntu.(none)>
2012-08-19 16:04:14 +0800

    fix c++ bug 1.assert 0<=level<4 2.check db and Table before delete or clean 3.filter . and .. 4.not test

--- src/FileKVDB.cc
+++ src/FileKVDB.cc
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <assert.h>
 #include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
@@ -15,45 +16,47 @@ using namespace std;
 string FileKVDB::dbPathName[4] = { "DB->root:", "DB->DB:", "DB->Table:", "DB->item:" };
 string FileKVDB::dbPrefix[4] = {"", "DB_", "T_", "K_" };
 string FileKVDB::version = "0.0.4";
-	
+  
 string FileKVDB::validK(string K)
 {
-	while(1) {
-		int pos = K.find('/');
-		if( pos!= K.npos ) {
-			K=K.substr(0,pos-1)+K.substr(pos+1);
-		}
-		else
-			break;
-	}
-	return K;
+  while(1) {
+    int pos = K.find('/');
+    if( pos!= K.npos ) {
+      K=K.substr(0,pos-1)+K.substr(pos+1);
+    }
+    else
+      break;
+  }
+  return K;
 }
 string FileKVDB::joinPath( int level )
 {
-	string str = "";
-	for( int i=0;i<level;i++ ) {
-		str.append(dbPath[i]+'/');
-	}
-	return str;
+  assert( level>=0 && level<4 );
+  string str = "";
+  for( int i=0;i<level;i++ ) {
+    str.append(dbPath[i]+'/');
+  }
+  return str;
 }
 vector<string>& FileKVDB::internalList( int level )
 {
-	string dir = joinPath(level);
-	vector<string>* files = dirAdir( dir );
+  assert( level>=0 && level<4 );
+  string dir = joinPath(level);
+  vector<string>* files = dirAdir( dir );
   vector<string> *ret = new vector<string>;
-	for( int k=0;k<files->size();k++ ) {
-		string name = realName( (*files)[k], 3 );
-		if( name.find(".tar.gz")==name.npos ) {
-			ret->push_back(name);
-		}
-	}
-	delete files;
-	return *ret;	
+  for( int k=0;k<files->size();k++ ) {
+    string name = realName( (*files)[k], 3 );
+    if( name.find(".tar.gz")==name.npos ) {
+      ret->push_back(name);
+    }
+  }
+  delete files;
+  return *ret;  
 }
 
 vector<string>* FileKVDB::dirAdir( string dir )
 {
-	vector<string>* files = new vector<string>;
+  vector<string>* files = new vector<string>;
     DIR *dp;
     struct dirent *dirp;
     if((dp  = opendir(dir.c_str())) == NULL) {
@@ -62,153 +65,173 @@ vector<string>* FileKVDB::dirAdir( string dir )
     }
 
     while ((dirp = readdir(dp)) != NULL) {
-        files->push_back(string(dirp->d_name));
+        string sdir = string(dirp->d_name);
+        if( sdir==".." || sdir=="." )
+          continue;
+        files->push_back(sdir);
     }
     closedir(dp);
     return files;
 }
 void FileKVDB::newDir( int level, string d )
 {
-	string dir = joinPathWithPrefix(level)+d;
-	mkdir( dir.c_str(),0755);
+  assert( level>=0 && level<4 );
+  string dir = joinPathWithPrefix(level)+d;
+  mkdir( dir.c_str(),0755);
 }
 void FileKVDB::clearBackup( string dir )
 {
-	vector<string> *files = dirAdir( dir );
-	vector<string> *ret = new vector<string>;
-	for( int k=0;k<files->size();k++ ) {
-		string name = realName( (*files)[k], 3 );
-		if( name.find(".tar.gz")!=name.npos ) {
-			string fullname = dir+(*files)[k];
-			remove(fullname.c_str());
-		}
-	}
-	delete files;
-	delete ret;
+  vector<string> *files = dirAdir( dir );
+  vector<string> *ret = new vector<string>;
+  for( int k=0;k<files->size();k++ ) {
+    string name = realName( (*files)[k], 3 );
+    if( name.find(".tar.gz")!=name.npos ) {
+      string fullname = dir+(*files)[k];
+      remove(fullname.c_str());
+    }
+  }
+  delete files;
+  delete ret;
 }
 
 FileKVDB::FileKVDB( string root )
 {
-	dbPath[0]=root;
+  dbPath[0]=root;
 }
 
 string FileKVDB::clearTable( string Table, string db )
 {
-	string dir = dbPath[0]+'/'
-		+ dbPrefix[1]+db+'/'
-		+ dbPrefix[1]+Table+'/';
-	vector<string> *files = dirAdir( dir );
-	for( int i=0;i<files->size();i++ )
-	{
-		string fullname = dir+(*files)[i];
-		remove(fullname.c_str());
-	}
-	delete files;		
-	return dir;
+  if( hasDB( db ) ) {
+    string backupDB = DB();
+    DB(db);
+    if( hasTable( Table ) ) {
+      string dir = dbPath[0]+'/'
+        + dbPrefix[1]+db+'/'
+        + dbPrefix[1]+Table+'/';
+      vector<string> *files = dirAdir( dir );
+      for( int i=0;i<files->size();i++ ) {
+        string fullname = dir+(*files)[i];
+        remove(fullname.c_str());
+      }
+      delete files;    
+      return dir;
+    }
+    DB( backupDB );
+  }
+  return "";
 }
 void FileKVDB::delTable( string Table, string db )
 {
-	string dir = clearTable( Table, db );
-	remove( dir.c_str() );
+  if( hasDB( db ) ) {
+    string backupDB = DB();
+    DB(db);
+    if( hasTable( Table ) ){
+      string dir = clearTable( Table, db );
+      if( dir != "" )
+        remove( dir.c_str() );
+    }
+    DB( backupDB );
+  }
 }
 void FileKVDB::clearDB( string db )
 {
-	string backupDB = DB();
-	DB(db);
-	vector<string> tables = listDB();
-	for( int i=0;i<tables.size();i++ )
-	{
-		delTable(tables[i],db);	
-	} 
-	clearTableBackup(db);
-	DB(backupDB);
+  if( hasDB( db ) ) {
+    string backupDB = DB();
+    DB(db);
+    vector<string> tables = listDB();
+    for( int i=0;i<tables.size();i++ ) {
+      delTable(tables[i],db);  
+    } 
+    clearTableBackup(db);
+    DB(backupDB);
+  }
 }
-		
+    
 void FileKVDB::set( string K, string V )
 {
-	string filename = fn(K);
-	ofstream out(filename.c_str());
-	out<<V;
-	out.close(); 
+  string filename = fn(K);
+  ofstream out(filename.c_str());
+  out<<V;
+  out.close(); 
 }
 string FileKVDB::get( string K )
 {
-	string filename = fn(K);
-	ifstream in(filename.c_str());
-	string V = "";
-	in.unsetf(ios::skipws); 
-	do
-	{
-		char a;
-		in>>a;
-		V+=a;
-	} while (!in.eof());
-	V=V.substr(0,V.length()-1);
-	in.close();
-	return V; 
+  string filename = fn(K);
+  ifstream in(filename.c_str());
+  string V = "";
+  in.unsetf(ios::skipws); 
+  do {
+    char a;
+    in>>a;
+    V+=a;
+  } while (!in.eof());
+  V=V.substr(0,V.length()-1);
+  in.close();
+  return V; 
 }
 
 void FileKVDB::del( string K )
 {
-	string filename = fn(K);
-	remove(filename.c_str());
+  if( has(K) ) {
+    string filename = fn(K);
+    remove(filename.c_str());
+  }
 }
 bool FileKVDB::has( string K )
 {
-	string filename = fn(K);
-	ifstream in(filename.c_str());
-	if(in) {
-		in.close();
-		return true;
-	}
-	return false;
+  string filename = fn(K);
+  ifstream in(filename.c_str());
+  if(in) {
+    in.close();
+    return true;
+  }
+  return false;
 }
 
 bool FileKVDB::hasDir( int level, string d )
 {
-	string dir = joinPathWithPrefix(level)+d;
-	DIR *pDir;
-    bool bExists = false;
-    pDir = opendir (dir.c_str());
-    if (pDir != NULL)
-    {
-        bExists = true;    
-        (void) closedir (pDir);
-    }
-
-    return bExists;
+  assert( level>=0 && level<4 );
+  string dir = joinPathWithPrefix(level)+d;
+  DIR *pDir;
+  bool bExists = false;
+  pDir = opendir (dir.c_str());
+  if (pDir != NULL) {
+    bExists = true;    
+    (void) closedir (pDir);
+  }
+  return bExists;
 }
 
 
 #ifdef SELFTEST
 void TESTSet() {
-	FileKVDB ttu("kvdb");
-	ttu.newDB("myDB");
-	ttu.DB("myDB");
-	ttu.newTable("myTable");
-	ttu.Table("myTable");
-	ttu.set("1","Hello World!- ");
+  FileKVDB ttu("kvdb");
+  ttu.newDB("myDB");
+  ttu.DB("myDB");
+  ttu.newTable("myTable");
+  ttu.Table("myTable");
+  ttu.set("1","Hello World!- ");
 }
 
 void TESTGet() {
-	FileKVDB ttu("kvdb");
-	if( ttu.hasDB("myDB") ) {
-		ttu.DB("myDB");
-		if( ttu.hasTable("myTable") ) {
-			ttu.Table("myTable");
-			if( ttu.has("1") ) {
-				string V = ttu.get("1");
-				cout << "Get :" << V <<'|' << endl;
-				return;
-			}
-		}
-	}
-	cout << "Get Error" << endl;
+  FileKVDB ttu("kvdb");
+  if( ttu.hasDB("myDB") ) {
+    ttu.DB("myDB");
+    if( ttu.hasTable("myTable") ) {
+      ttu.Table("myTable");
+      if( ttu.has("1") ) {
+        string V = ttu.get("1");
+        cout << "Get :" << V <<'|' << endl;
+        return;
+      }
+    }
+  }
+  cout << "Get Error" << endl;
 }
 
 int main() {
-	TESTSet();
-	TESTGet();
+  TESTSet();
+  TESTGet();
 }
 
 #endif
--- src/FileKVDB.h
+++ src/FileKVDB.h
@@ -1,51 +1,51 @@
 #ifndef __FILEKVDB_H
 #define __FILEKVDB_H
 class FileKVDB {
-	private:
-		string dbPath[4];
-		static string dbPathName[];
-		static string dbPrefix[];
-		string validK(string K);
-		void setDB(string db) {return DBPath(1,db);};
-		string setDB() {return DBPath(1);};
-		void setTable(string t) {return DBPath(2,t);};
-		string setTable() {return DBPath(2);};
-		string joinPath( int level );
-		string joinPathWithPrefix( int level ) { return joinPath(level)+dbPrefix[level];};
-		string DBPath( int level ){return realName( dbPath[level], level );};
-		void DBPath( int level, string mypath ){dbPath[level]=dbPrefix[level]+mypath;};
-		string realName ( string str, int level ){int pos=str.find('_');if(pos!=str.npos) return str.substr(pos+1); else return str;};
-		void newDir( int level, string dir );
-		bool hasDir( int level, string dir );
-		vector<string>& internalList( int level );
-		void clearBackup( string dir );
-		vector<string>* dirAdir( string dir );
+  private:
+    string dbPath[4];
+    static string dbPathName[];
+    static string dbPrefix[];
+    string validK(string K);
+    void setDB(string db) {return DBPath(1,db);};
+    string setDB() {return DBPath(1);};
+    void setTable(string t) {return DBPath(2,t);};
+    string setTable() {return DBPath(2);};
+    string joinPath( int level );
+    string joinPathWithPrefix( int level ) { assert( level>=0 && level<4 );return joinPath(level)+dbPrefix[level];};
+    string DBPath( int level ){ assert( level>=0 && level<4 );return realName( dbPath[level], level );};
+    void DBPath( int level, string mypath ){ assert( level>=0 && level<4 );dbPath[level]=dbPrefix[level]+mypath;};
+    string realName ( string str, int level ){ assert( level>=0 && level<4 );int pos=str.find('_');if(pos!=str.npos) return str.substr(pos+1); else return str;};
+    void newDir( int level, string dir );
+    bool hasDir( int level, string dir );
+    vector<string>& internalList( int level );
+    void clearBackup( string dir );
+    vector<string>* dirAdir( string dir );
 
-	public:
-		FileKVDB( string root );
-		static string version;
-		string fn( string k ){	return joinPathWithPrefix(3)+validK(k);};
-		void root( string aRoot ) {return DBPath( 0, aRoot );};
-		string root() { return DBPath(0); };
-		void DB( string db ) { return setDB(db); };
-		string DB() { return setDB(); };
-		void Table( string table ) {return setTable( table );}; 
-		string Table() { return setTable(); };
-		void newDB( string db ) { newDir( 1, db ); };
-		void newTable( string table ) {	newDir( 2, table );};
-		bool hasDB(string db) {	return hasDir(1,db); };
-		bool hasTable(string table) { return hasDir(2,table); };
-		string clearTable( string Table, string db );
-		void delTable( string Table, string db );
-		void clearDB( string db );
-		void clearTableBackup( string db ) {clearBackup(dbPath[0]+'/'+dbPrefix[1]+db+'/');};
-		void clearDBBackup() {clearBackup(dbPath[0]+'/');};
-		vector<string> list() {return internalList(3);};
-		vector<string> listTable() {return internalList(2);};
-		vector<string> listDB() {	return internalList(1); };
-		void set( string K, string V );
-		string get( string K );
-		void del( string K );
-		bool has( string K );
+  public:
+    FileKVDB( string root );
+    static string version;
+    string fn( string k ){  return joinPathWithPrefix(3)+validK(k);};
+    void root( string aRoot ) {return DBPath( 0, aRoot );};
+    string root() { return DBPath(0); };
+    void DB( string db ) { return setDB(db); };
+    string DB() { return setDB(); };
+    void Table( string table ) {return setTable( table );}; 
+    string Table() { return setTable(); };
+    void newDB( string db ) { newDir( 1, db ); };
+    void newTable( string table ) {  newDir( 2, table );};
+    bool hasDB(string db) {  return hasDir(1,db); };
+    bool hasTable(string table) { return hasDir(2,table); };
+    string clearTable( string Table, string db );
+    void delTable( string Table, string db );
+    void clearDB( string db );
+    void clearTableBackup( string db ) {clearBackup(dbPath[0]+'/'+dbPrefix[1]+db+'/');};
+    void clearDBBackup() {clearBackup(dbPath[0]+'/');};
+    vector<string> list() {return internalList(3);};
+    vector<string> listTable() {return internalList(2);};
+    vector<string> listDB() {  return internalList(1); };
+    void set( string K, string V );
+    string get( string K );
+    void del( string K );
+    bool has( string K );
 };
 #endif
